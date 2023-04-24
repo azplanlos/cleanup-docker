e3b00d784b1acbe109a7456008080b3dd5de8c1d
Andreas Zöllner <andi@go-for-fun.de>
2023-04-24 17:34:21 +0200

    skip first cleanup to make sure all services are started

--- docker-entrypoint.ps1
+++ docker-entrypoint.ps1
@@ -1,11 +1,12 @@
 $CLEANUP_RATE = $env:CLEANUP_RATE
 
 while ($true) {
+    Start-Sleep -Seconds $CLEANUP_RATE
+
     Write-Host "** Starting cleanup."
 
     . ./cleanup.ps1
 
     Write-Host "** Cleanup done."
     Write-Host "** Sleeping for: ${CLEANUP_RATE} seconds"
-    Start-Sleep -Seconds $CLEANUP_RATE
 }
